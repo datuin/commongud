30b8b55258c65b40b19f11aab48056f0e3e5d787
Retchiele <rldatuin@gmail.com>
2020-12-29 12:37:30 -0800

    fixed footer nav, deleted extra about.html

--- gud_app/urls.py
+++ gud_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('us', views.index),
     path('us/men', views.men),
     path('us/women', views.women),
-    path('us/mission', views.mission),
+    path('mission', views.mission),
     path('registration',views.registration_template),
     path('signup', views.registration_user),
     path('login', views.login_user),
