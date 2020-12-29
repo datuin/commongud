32668a7279c7d761997f268ac56790b660bcf4f1
Retchiele <rldatuin@gmail.com>
2020-12-28 21:04:41 -0800

    contact

--- gud_app/urls.py
+++ gud_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('registration',views.registration_template),
     path('signup', views.registration_user),
     path('login', views.login_user),
+    path('contact', views.contact),
     path('dashboard/orders', views.orders),
     path('dashboard/orders/show', views.orderDetail),
     path('dashboard/products', views.products),
